ccb547a696bab5469eaeeec51b33fb32d2d2abbe
Ogaros <frost3000@hotmail.ru>
2014-09-08 13:14:09 +0400

    reading/writing settings is now functional

--- settings.h
+++ settings.h
@@ -1,20 +1,22 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 #include "QString"
+#include "QSettings"
+#include "QApplication"
+#include "QFile"
 
 class Settings
 {
 public:
-    enum repeatedDisplay{All, Future, Closest};
+    enum repeatedDisplay{All = 0, Future = 1, Closest = 2};
     bool autorun;
     QString dateFormat;
     repeatedDisplay rDisplay;
-    void reset()
-    {
-        autorun = true;
-        dateFormat = "dd/MM/yyyy";
-        rDisplay = repeatedDisplay::Future;
-    }
+    void restore();
+    void load();
+    void save();
+private:
+    const QString filePath = QApplication::applicationDirPath() + "/settings.ini";
 };
 
 #endif // SETTINGS_H
