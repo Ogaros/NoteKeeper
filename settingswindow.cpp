aea855a3a3c9c25e3febbdd9edf22d848cae4c3e
Ogaros <frost3000@hotmail.ru>
2014-09-22 13:51:57 +0400

    Added tooltip for date format setting

--- settingswindow.cpp
+++ settingswindow.cpp
@@ -7,9 +7,25 @@ SettingsWindow::SettingsWindow(const std::weak_ptr<Settings> settings, QWidget *
 {
     this->settings = settings.lock();
     ui->setupUi(this);
-    QRegExpValidator *dateValidator = new QRegExpValidator(QRegExp("((d{0,4}|M{0,4}|y{0,4})[/\\s\\.\\:\\\\])*"));
+    QRegExpValidator *dateValidator = new QRegExpValidator(QRegExp("((d{0,4}|M{0,4}|y{0,4})[/\\s\\.\\,\\:\\\\])*"));
     //TODO: add extra y when user inputs y
-    ui->dateFormatEdit->setValidator(dateValidator);
+    ui->dateFormatEdit->setValidator(dateValidator);    
+    ui->iconLabel->setPixmap(QIcon(":/images/qMarkIcon").pixmap(20,20));
+    ui->iconLabel->setToolTip("Date format can be formedwith the following expressions:<br>"
+                              "<table>"
+                              "<tr><td><b>d</b></td><td>the day as number without a leading zero (1 to 31)</td></tr>"
+                              "<tr><td><b>dd</b></td><td>the day as number with a leading zero (01 to 31)</td></tr>"
+                              "<tr><td><b>ddd</b></td><td>the abbreviated day name ('Mon' to 'Sun')</td></tr>"
+                              "<tr><td><b>dddd</b></td><td>the long day name ('Monday' to 'Sunday')</td></tr>"
+                              "<tr><td><b>M</b></td><td>the month as number without a leading zero (1 to 12)</td></tr>"
+                              "<tr><td><b>MM</b></td><td>the month as number with a leading zero (01 to 12)</td></tr>"
+                              "<tr><td><b>MMM</b></td><td>the abbreviated month name ('Jan' to 'Dec')</td></tr>"
+                              "<tr><td><b>MMMM &nbsp;</b></td><td>the long month name ('January' to 'December')</td></tr>"
+                              "<tr><td><b>yy</b></td><td>the year as two digit number (00 to 99)</td></tr>"
+                              "<tr><td><b>yyyy</b></td><td>the year as four digit number (1993)</td></tr>"
+                              "</table>"
+                              "<br><br>You can use white spaces and . , : / \\ symbols to separate"
+                              "<br>the expressions.");
     setConnections();
     loadSettings();
 }
