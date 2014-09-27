f5dba0538493ea1684cd26ae6e4e91b5001523ef
Ogaros <frost3000@hotmail.ru>
2014-09-27 19:55:14 +0400

    Removed .cpp file

--- note.cpp
+++
@@ -1,5 +0,0 @@
-#include "note.h"
-
-Note::Note(const QDate date, const QString text, const nFrequency frequency,
-           const bool notifEnabled, const int daysPrior):
-           date(date), text(text), frequency(frequency), notifEnabled(notifEnabled), daysPrior(daysPrior){}
